22e02b7ed46aad578ea6138a0bbc3f9ddb3ce1f8
Briana Wright <brianaleew@gmail.com>
2023-03-02 09:52:55 -0500

    Reworking the navbar

---
+++ ecommerce-product-page-main/js/app.js
@@ -0,0 +1,8 @@
+const hamburger = document.querySelector(".hamburger");
+const navList = document.querySelector(".nav-list");
+
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active")
+    navList.classList.toggle("active")
+})
