26d675d8e55fb4d122ce736c10c0b7e74f0e7e20
Briana Wright <brianaleew@gmail.com>
2023-03-06 09:27:54 -0500

    finally got modal to show up and close. needs to be styled

--- ecommerce-product-page-main/js/app.js
+++ ecommerce-product-page-main/js/app.js
@@ -1,8 +1,34 @@
 const hamburger = document.querySelector(".hamburger");
 const navList = document.querySelector(".nav-list");
+const thumbnails = document.querySelectorAll(".product-images-thumbnails")
+const modal = document.getElementById("modal");
+const modalClose = document.getElementById("close")
 
 
-hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active")
-    navList.classList.toggle("active")
-})
+const openModal = () => {
+    modal.classList.add("open")
+    console.log('clicked')
+}
+
+const closeModal = () => {
+    modal.classList.remove("open")
+    console.log('clicked to close')
+}
+
+
+
+
+
+// hamburger.addEventListener("click", () => {
+//     hamburger.classList.toggle("active")
+//     navList.classList.toggle("active")
+// })
+
+
+
+
+for (i = 0; i < thumbnails.length; i++) {
+    thumbnails[i].addEventListener("click", openModal)
+}
+
+modalClose.addEventListener("click", closeModal)
